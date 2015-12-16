acbca99c9bce40f0d3b9efa33fc5323151858b8f
joemoe <johannes@crate.io>
2015-12-16 18:21:32 +0100

    added routes

--- php/index.php
+++ php/index.php
@@ -1,10 +1,50 @@
 <?php
 require 'vendor/autoload.php';
 
+
 $app = new \Slim\Slim();
 
 $app->get('/', function() {
-	echo 'test';
+	echo json_encode(array());
 });
+
+
+// posts
+
+$app->post('/posts', function() {
+	echo json_encode(array());
+});
+
+$app->get('/posts', function() {
+	echo json_encode(array());
+});
+
+$app->put('/posts/:id', function($id) {
+
+});
+
+$app->delete('/posts/:id', function($id) {
+
+});
+
+$app->put('/posts/:id/likes', function($id) {
+	echo json_encode(1);
+});
+
+
+// images
+
+$app->get('/images', function() {
+	echo json_encode(array());
+});
+
+$app->post('/images', function() {
+	echo json_encode(array());
+});
+
+$app->get('/image/:digest', function($digest) {
+
+});
+
 
 $app->run();
